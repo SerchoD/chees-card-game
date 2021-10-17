070a6c534d1be6db288c46b1698a4ec530e61b2d
Sercho <diaz.sergio@protonmail.com>
2021-10-17 08:39:49 -0300

    Save - queda pendiente hacer que una misma Card tenga varios Estilos.

--- src/components/Card-game/Card.jsx
+++ src/components/Card-game/Card.jsx
@@ -3,13 +3,20 @@ import './card.css';
 
 const Card = ({
 	title = 'Titulo',
-	description = 'Este es el lugar donde debería estar la descripción si la hubiera, pero como no la hay, en su lugar hay un texto de prueba que representa una potencial descripción de extensa longitud.',
+	description = 'Este es el lugar donde debería estar la descripción si la hubiera, pero como no la hay, en su lugar hay un texto de prueba que representa una potencial descripción de extensa longitud. Este es el lugar donde debería estar la descripción si la hubiera, pero como no la hay, en su lugar hay un texto de prueba que representa una potencial descripción de extensa longitud',
+	cardClass = ['normal', 'special', 'elit'],
 }) => {
 	return (
 		<div className='card'>
 			<h1 className='title'>{title}</h1>
-			<span className='lane1'></span>
-			<p className='description'>{description}</p>
+
+			<p className='description' title={description}>
+				{description}
+			</p>
+
+			<div className='button'>
+				<button className='btn-delete'>Descartar</button>
+			</div>
 		</div>
 	);
 };
